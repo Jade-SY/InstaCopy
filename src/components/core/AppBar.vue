102d1c2eb730ba79902d84d3828bc23936a02145
Jase-SY <jadesy90@gmail.com>
2020-10-15 17:52:31 +0900

    add files

---
+++ src/components/core/AppBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-app-bar color="white" elevation="0">
+      <h1 class="logo">
+        <img
+          src="https://fontmeme.com/images/instagram-new-logo.png"
+          width="100%"
+          alt="logo"
+        />
+      </h1>
+      <v-spacer></v-spacer>
+      <div class="icon-box">
+        <v-btn color="black" depressed icon>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-btn color="black" depressed icon>
+          <v-icon>mdi-send-outline</v-icon>
+        </v-btn>
+        <v-btn color="black" depressed icon>
+          <v-icon>mdi-compass-outline</v-icon>
+        </v-btn>
+        <v-btn color="black" depressed icon>
+          <v-icon>mdi-heart-outline</v-icon>
+        </v-btn>
+        <v-btn depressed icon>
+          <v-avatar size="28">
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"
+          /></v-avatar>
+        </v-btn>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.v-application .white {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important ;
+}
+.logo {
+  width: 150px;
+  padding-top: 6px;
+}
+</style>
---
+++ src/components/main/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+};
+</script>
+
+<style lang="scss" scoped></style>
